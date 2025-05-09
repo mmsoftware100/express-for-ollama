a6ce9dea9483b1d3d292be532375072b51836d4b
Cisco Ramon <aungkoman@gmail.com>
2025-05-09 12:24:45 +0630

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Express of Ollama
+
---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// GET /hello
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// GET /api/versions
+app.get('/api/versions', (req, res) => {
+  res.json({ version: '1.0.0', name: 'Simple API' });
+});
+
+// POST /api/chat
+app.post('/api/chat', (req, res) => {
+  const data = req.body;
+  res.json({ message: data });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
